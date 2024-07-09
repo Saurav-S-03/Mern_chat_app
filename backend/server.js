70cd926873d11886d008e955e1e73bfea4da2fa9
Saurav-S-03 <mailtosauravs@gmail.com>
2024-07-09 20:48:23 +0530

    Chat APIs coded

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 
@@ -26,6 +28,8 @@ app.get('/', (req,res) => {
 // })
 
 app.use('/api/user',userRoutes);
+
+app.use('/api/chat',chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
